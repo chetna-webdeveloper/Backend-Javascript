fd1920da46669686af54d0025944efa26fcabdf7
chetna <chetnachetna8585@gmail.com>
2024-09-16 13:47:42 +0530

    db connected

---
+++ src/index.js
@@ -0,0 +1,46 @@
+import connectDB from "./db/index.js";
+import dotenv  from 'dotenv'
+
+
+dotenv.config({
+    path:"./env"
+})
+
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import express from "express"
+const app = express()
+
+
+    ; (async () => {
+        try {
+            await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+            app.on("error", (error) => {
+                console.log("ERRR : ", error)
+                throw error
+            })
+            app.listen(process.env.PORT, () => {
+                console.log(`App is Listening on port ${process.env.PORT}`)
+            })
+        } catch (error) {
+            console.log("ERROR: ", error)
+            throw error
+        }
+    })()*/
